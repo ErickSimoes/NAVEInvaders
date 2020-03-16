b8fe7f41555e64c50a308dd45d4087fc6609e2bb
Erick Simões <erick.simoes@live.com>
2020-03-16 11:24:02 -0300

    Add basic movimentation of Player with rigidbody

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerBehaviour : MonoBehaviour {
+
+    public float speed;
+    Rigidbody rigidbody;
+
+    void Start() {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    void Update() {
+        
+    }
+
+    private void FixedUpdate() {
+        rigidbody.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, 0f);
+    }
+}
