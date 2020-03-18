b686d47c278b12078c43f42377cba803feafa5c7
Erick Simões <erick.simoes@live.com>
2020-03-18 17:13:57 -0300

    Add basic mechanics of enemy

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class EnemyBehaviour : MonoBehaviour {
+
+    public float speed;
+    public GameObject laser;
+    Rigidbody rb;
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        InvokeRepeating("Shoot", 1f, 2f);
+    }
+
+    void Update() {
+
+    }
+
+    void FixedUpdate() {
+        rb.velocity = Vector3.down * speed;
+    }
+
+    void Shoot() {
+        Instantiate(laser, transform.position, Quaternion.identity).GetComponent<LaserBehaviour>().isUp = false;
+    }
+}
